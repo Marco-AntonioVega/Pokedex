ca8772eded35a22e6ae71cb58c720d2d09b992f2
Marco-Antonio Vega <27344marcovega@gmail.com>
2023-04-04 13:37:45 -0700

    init commit

--- Pokedex/AppDelegate.swift
+++ Pokedex/AppDelegate.swift
@@ -1,19 +1,30 @@
 //
 //  AppDelegate.swift
-//  Pokedex
+//  lab-insta-parse
 //
-//  Created by Marco-Antonio Vega on 4/4/23.
+
 //
 
 import UIKit
+import ParseSwift
+// TODO: Pt 1 - Import Parse Swift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // TODO: Pt 1 - Initialize Parse SDK
+        // https://github.com/parse-community/Parse-Swift/blob/main/ParseSwift.playground/Sources/Common.swift
+        ParseSwift.initialize(applicationId: "qh4VPIg8wga49MjNwCGLOXxjx151qYWxKlaY5d8j",
+                              clientKey: "isVr7EMSxxkOcQHuPtxFFDBDElKhLwyuVioRxitY",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
+
+
+        // TODO: Pt 1: - Instantiate and save a test parse object to your server
+        // https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L121
+        
         return true
     }
 
@@ -30,7 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+// TODO: Pt 1 - Create Test Parse Object
+// https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L33
