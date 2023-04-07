8db70f39edcd8db2bab13490014f9036b6ff3ad0
Jose Castro <chepe.castro2312@gmail.com>
2023-04-07 11:19:15 -0700

    Random tab implementation

--- Pokedex/ViewControllers/RandomViewController.swift
+++ Pokedex/ViewControllers/RandomViewController.swift
@@ -6,17 +6,55 @@
 //
 
 import UIKit
+import AVFoundation
+import Nuke
 
 class RandomViewController: UIViewController {
 
+    @IBOutlet weak var variants: UISegmentedControl!
+    @IBOutlet weak var pokemon_Name: UILabel!
+    @IBOutlet weak var pokemon_Image: UIImageView!
+    @IBOutlet weak var flavor_text: UITextView!
+    @IBOutlet weak var type: UILabel!
+    @IBOutlet weak var species: UILabel!
+    @IBOutlet weak var height: UILabel!
+    @IBOutlet weak var weight: UILabel!
+    @IBOutlet weak var abilities: UILabel!
+    
+    private var player: AVPlayer?
+    
+    func playAudio (from url: URL){
+        player = AVPlayer (url: url)
+        player?.play()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         APIFunctions.getNationalDexCap();
+        
+        let url = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/144.png")
+        pokemon_Name.text = String("Articuno #0144")
+        
+        Nuke.loadImage(with: url!, into: pokemon_Image)
+        flavor_text.text = String("A legendary bird POKéMON that is said to appear to doomed people who are lost in icy mountains.")
+        
+        type.text = String("Psychic/Flying")
+        species.text = String("Freeze POKéMON")
+        height.text = String("1.7 m")
+        weight.text = String("50.9 kg")
+        abilities.text = String("Competitive")
         // Do any additional setup after loading the view.
     }
     
     @IBAction func onLogOutTapped(_ sender: Any) {
         showConfirmLogoutAlert()
+    }
+    
+    @IBAction func playCryOnTapped(_ sender: Any) {
+        let audioURL = URL(string: "https://pokemoncries.com/cries/144.mp3")
+        if let audioURL = audioURL{
+            playAudio(from: audioURL)
+        }
     }
     
     private func showConfirmLogoutAlert() {
@@ -30,14 +68,4 @@ class RandomViewController: UIViewController {
         present(alertController, animated: true)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
